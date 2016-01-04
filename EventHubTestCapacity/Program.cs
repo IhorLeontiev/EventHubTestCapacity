e22c39ae767a00ec3d4c98f12faf2a20a10448f2
Леонтье Игорь Андреевич <leontuev@rambler.ru>
2016-01-04 11:31:09 +0100

    Main

---
+++ EventHubTestCapacity/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+using System.Diagnostics;
+
+namespace EventHubTestCapacity
+{
+    class Program
+    {
+        static string eventHubName = "";
+        static string connectionString = ";
+        static void Main(string[] args)
+        {
+
+
+            System.Net.ServicePointManager.DefaultConnectionLimit = 1024;
+            Console.WriteLine("Start sending ...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Paralelize();
+            sw.Stop();
+            Console.WriteLine("Completed in {0} ms", sw.ElapsedMilliseconds);
+            Console.WriteLine("Press enter key to stop worker.");
+            Console.ReadLine();
+
+        }
+
+
+        static void Paralelize()
+        {
+            Task[] tasks = new Task[25];
+            for (int i = 0; i < 25; i++)
+            {
+                tasks[i] = new Task(() => Send(500));
+            }
+
+            Parallel.ForEach(tasks, (t) => { t.Start(); });
+            Task.WaitAll(tasks);
+        }
+
+        public static void Send(int eventCount)
+        {
+            Sender s = new Sender(connectionString, eventHubName, eventCount);
+            s.SendingRandomMessages();
+        }
+
+
+   
+    }
+}
---
+++ EventHubTestCapacity/Sender.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace EventHubTestCapacity
+{
+    class Sender
+    {
+        string connectionString;
+        string eventHubName;
+        int eventCount;
+
+        public Sender(string connectionString, string eventHubName, int eventCount)
+        {
+            this.connectionString = connectionString;
+            this.eventCount = eventCount;
+            this.eventHubName = eventHubName;
+        }
+
+        public void SendingRandomMessages()
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+           for(int i=0;i< eventCount; i++)
+                try
+                {
+                    var message = Guid.NewGuid().ToString();
+                    Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, message);
+                    eventHubClient.Send(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                    Console.ResetColor();
+                }
+
+               Thread.Sleep(200);
+            }
+        }
+    }
